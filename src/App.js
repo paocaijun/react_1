fe9c9de2b9b2f256a57f44cbd527658d4139127f
zr_dengxue <1097133606@qq.com>
2019-01-16 14:04:07 +0800

    fea:redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react'
-// class是定义组件关键字
-// App 组件的名称 必须是以大写字母开头
-// React.Component 是组件的父类 （基本的组件）
-// extends是继承
+import React, { component } from "react"
 class App extends React.Component {
-    // 组件必须要有返回值
-    // render是渲染组件的函数 渲染的时候必须有返回值
-    render() {
-        return <div className='hello'>hello world22!</div>
-    }
+	constructor(props, context) {
+		super(props)
+		this.state = {
+			name: "app_component",
+			toTop: false,
+			countNum: 10
+		}
+		this.toCount = this.toCount.bind(this)
+	}
+	toCount() {
+		console.log("this", this)
+		this.setState(preState => ({
+			countNum: preState.countNum + 5
+		}))
+	}
+	componentWillMount() {
+		console.log("mount", this)
+	}
+	render() {
+		return (
+			<div className="hello" onClick={this.toCount}>
+				hello world{this.state.countNum}
+			</div>
+		)
+	}
 }
-
-// 组件如果需要在别的地方使用 需要导出
-
 export default App
--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
-
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.js";
-import '@/layouts/css/index.scss'
-ReactDOM.render(<App />, document.getElementById("app"));
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App.js"
+import "@/layouts/css/index.scss"
+import { createStore } from "redux"
+const rootReducer = function(state, action) {
+	if (typeof state === "undefined") {
+		return 0
+	}
+	switch (action.type) {
+		case "add_five":
+			return state + 5
+		case "add_ten":
+			return state + 10
+		default:
+			return state
+	}
+	return state
+}
+// import {rootReducer} from './reducers'
+import { Provider } from "react-redux"
+const store = createStore(rootReducer)
+console.log("store", store)
+ReactDOM.render(
+	<div>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</div>,
+	document.getElementById("app")
+)
 // ReactDOM.render(<div>Hello World1111</div>, document.getElementById("app"));
