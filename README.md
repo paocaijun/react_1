d6cbcf73e9f365a20cf75912bb576fb3ee67aac3
zr_dengxue <1097133606@qq.com>
2019-03-09 19:01:17 +0800

    webpack

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # react_1
 
 使用 scss,但是不需要安装 scss，安装 sass,css,style 的 loader+node-sass
+
+在 index.html 内引入打包后的包，写绝对路径，不要写相对路径，但是引入 htmlWebpackPlugin 之后不需要自己引入 js 文件了？？？
--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,73 @@
-var webpack = require('webpack')
-const path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+var webpack = require('webpack');
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: __dirname + '/src/main.js', //指明编译开始的入口
-	output: {
-		path: path.join(__dirname, 'bundle'), //指明编译好的文件所在目录
-		publicPath: '',
-		filename: 'bundle.js'
-	},
-	resolve: {
-		alias: {
-			'@': path.join(__dirname, 'src')
-		},
-		extensions: ['.js', '.jsx', '.css', '.scss', '.json'] // 没带文件后缀时的匹配
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			inject: false,
-			filename: './index.html',
-			template: path.join(__dirname, 'index.html')
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.js|jsx$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				query: {
-					plugins: [
-						['import', { libraryName: 'antd', style: 'css' }] //antd按需加载
-					],
-					presets: ['es2015', 'react', 'stage-2']
-				}
-			},
-			{
-				test: /\.css$/,
-				exclude: /node_modules/,
-				loader: 'style-loader!css-loader'
-			},
-			{
-				test: /\.scss$/,
-				use: ['style-loader', 'css-loader', 'sass-loader?sourceMap']
-			},
-			{
-				//antd样式处理
-				test: /\.css$/,
-				exclude: /src/,
-				use: [
-					{ loader: 'style-loader' },
-					{
-						loader: 'css-loader',
-						options: {
-							importLoaders: 1
-						}
-					}
-				]
-			},
-			{
-				test: /\.(png|jpg|gif|webp)$/,
-				loader: 'url-loader',
-				options: {
-					limit: 8192,
-					name: '[name].[ext]'
-				}
-			}
-		]
-	}
-}
+    entry: {
+        apps: __dirname + '/src/main.js'
+    }, //指明编译开始的入口
+    output: {
+        path: path.join(__dirname, 'dist'), //指明编译好的文件所在目录
+        publicPath: '',
+        filename: 'js/[name].js'
+    },
+    resolve: {
+        alias: {
+            '@': path.join(__dirname, 'src')
+        },
+        extensions: ['.js', '.jsx', '.css', '.scss', '.json'] // 没带文件后缀时的匹配
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: false, //head ,body
+            filename: 'index.html', //生成的文件名
+            template: path.join(__dirname, 'index.html')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    plugins: [
+                        ['import', { libraryName: 'antd', style: 'css' }] //antd按需加载
+                    ],
+                    presets: ['es2015', 'react', 'stage-2']
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader?sourceMap']
+            },
+            {
+                //antd样式处理
+                test: /\.css$/,
+                exclude: /src/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|webp)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 8192, //小于8192kb的转化为base64,否则采用url-loader处理
+                    name: 'img/[name].[ext]'
+                }
+            }
+        ]
+    }
+};
